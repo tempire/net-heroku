c45779407d05d57e818783afcdab80deb13ec863
Glen Hinkle <glen@empireenterprises.com>
2012-02-11 02:36:38 -0800

    initial commit

---
+++ lib/Net/Heroku.pm
@@ -0,0 +1,24 @@
+package Net::Heroku;
+use Modern::Perl +2012;
+
+1;
+
+=head1 NAME
+
+Net::Heroku - Heroku API
+
+=head1 DESCRIPTION
+
+Heroku API
+
+=head1 USAGE
+
+=head1 METHODS
+
+=head1 VERSION
+
+0.01
+
+=head AUTHOR
+
+Glen Hinkle C<tempire@cpan.org>
---
+++ t/heroku.t
@@ -0,0 +1,7 @@
+use Modern::Perl +2012;
+use Test::More;
+use Net::Heroku;
+
+ok 1;
+
+done_testing;
